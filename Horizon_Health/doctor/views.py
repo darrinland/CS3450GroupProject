d8b6c6ccc2e13549271df3c6039db161adaae189
darrinland <darrinland@gmail.com>
2018-02-27 23:47:10 -0700

    Get doctor side very barebones

--- Horizon_Health/doctor/views.py
+++ Horizon_Health/doctor/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.template import loader
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
 	template = loader.get_template('doctor/index.html')
-	return HttpResponse(template.render(request))
+	return HttpResponse(template.render({}, request))
