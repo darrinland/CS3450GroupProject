1ff391e9d1e0e2f05986861f13bb859860bead36
darrinland <darrinland@gmail.com>
2018-04-04 08:36:03 -0600

    Added doctor models

--- Horizon_Health/doctor/models.py
+++ Horizon_Health/doctor/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Doctor(models.Model):
 	name = models.CharField(max_length=200)
 
-class PatientRecord(models.Model):
+class Patient(models.Model):
+	last_name = models.CharField(max_length=200)
+	first_name = models.CharField(max_length=200)
 	doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+	
+class PatientRecord(models.Model):
+	patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE)
 	note = models.CharField(max_length=1000)
