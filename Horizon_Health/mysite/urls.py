adf39e8e5be11c17d4e1c2f5740c119d406e0101
intrepid <7chrispy7@gmail.com>
2018-04-04 22:24:45 -0600

    updated URLs to include login

--- Horizon_Health/mysite/urls.py
+++ Horizon_Health/mysite/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', include('polls.urls')),
+    path('', include('Login.urls')),
     path('polls/', include('polls.urls')),
     path('patient/', include('patient.urls')),
     path('admin/', admin.site.urls),
