0e0602bb4702aea73fb56159a7983fe8a1533946
intrepid <7chrispy7@gmail.com>
2018-04-17 13:47:15 -0600

    Added user information to the url on login, or create new user.

--- Horizon_Health/Login/views.py
+++ Horizon_Health/Login/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from .models import User
+from patient.models import Patient
 
 def index(request):
 	return render(request, 'Login/index.html')
@@ -14,18 +15,20 @@ def handleLogin(request):
 	user = User.objects.filter(user_name=request.POST['user_name'], password=request.POST['password']).first()
 	if user:
 		if user.userType == "patient":
-			return redirect('http://127.0.0.1:8000/patient/')
+			return redirect('http://127.0.0.1:8000/patient?user='+str(user.pk))
 		elif user.userType == "doctor":
-			return redirect('http://127.0.0.1:8000/doctor')
+			return redirect('http://127.0.0.1:8000/doctor?user='+str(user.pk))
 		elif user.userType == "secretary":
-			return redirect('http://127.0.0.1:8000/secretary')
+			return redirect('http://127.0.0.1:8000/secretary?user='+str(user.pk))
 	else:
 		return redirect('Login:failedLogin')
 
 def handleNewUser(request):
-	user = User(user_name=request.POST['user_name'], password=request.POST['password'], userType='patient');
+	user = User(user_name=request.POST['user_name'], password=request.POST['password'], userType='patient')
 	user.save()
-	return redirect('http://127.0.0.1:8000/patient/')
+	patient = Patient()
+	patient.save()
+	return redirect('http://127.0.0.1:8000/patient/?user='+str(user.pk))
 
 def failedLogin(request):
 	return HttpResponse("Login Failed.  Please go back and try again.")
